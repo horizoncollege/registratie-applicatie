130aeddf92a9a215ab941e209204a176b07c49d6
Chris <166389@student.horizoncollege.nl>
2024-06-14 09:57:00 +0200

    Projecten container
    - De container heeft nu ronde hoeken
    - De container wordt nu correct getoond op mobiel
    - De achtergrondkleur is nu hetzelfde als op andere pagina's

--- registratie-applicatie/resources/views/pages/projecten.blade.php
+++ registratie-applicatie/resources/views/pages/projecten.blade.php
@@ -6,18 +6,40 @@
             <h1>Projecten</h1>
         </header>
 
-        <!-- Content -->
+        {{-- Searchbar --}}
         <div class="top-content">
             <div class="searchbar">
                 <i class="fa-solid fa-magnifying-glass"></i>
                 <input type="text" placeholder="Zoeken">
             </div>
+            {{-- Nieuw project button --}}
             <a href="/aanmelden" class="button-new-project">
                 <i class="fa-solid fa-plus"></i>
                 Nieuw project
             </a>
         </div>
 
+        {{-- Projects --}}
+        <div class="projects">
+            <div class="header-row">
+                <div class="header-item">Projectnaam</div>
+                <div class="header-item">Opdrachtgever</div>
+                <div class="header-item">Beschrijving</div>
+                <div class="header-item">Datum</div>
+            </div>
+            <div class="project-row">
+                <div class="project-item">Project 2</div>
+                <div class="project-item">Abbiraam Sinnathamby</div>
+                <div class="project-item">Volledige herontwerp van de bedrijfswebsite.</div>
+                <div class="project-item">2023-05-20</div>
+            </div>
+            <div class="project-row">
+                <div class="project-item">Project 1</div>
+                <div class="project-item">Bruno Dijkhuis</div>
+                <div class="project-item">Lanceringscampagne voor nieuw product.</div>
+                <div class="project-item">2023-06-10</div>
+            </div>
+        </div>
 
         <section id="home" class="dashboard-section">
             <div class="card-container">
