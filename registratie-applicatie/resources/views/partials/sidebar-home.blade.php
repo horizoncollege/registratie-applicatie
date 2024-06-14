cadf40dc66db786dfc5849e72ccb6fc7c7a40883
Abbiraam Sinnathamby <169357@student.horizoncollege.nl>
2024-06-14 09:07:03 +0200

    Sidebar aangemaakt voor homepagina
    Ik heb 2 verschillende sidebars aangemaakt een voor home en een voor dashboard #21

---
+++ registratie-applicatie/resources/views/partials/sidebar-home.blade.php
@@ -0,0 +1,20 @@
+<div class="top-bar">
+    <div class="top-bar-image">
+        <img src="images/talland-college-logo.png" alt="Logo" class="logo">
+    </div>
+</div>
+<div class="hamburger">
+    <div></div>
+    <div></div>
+    <div></div>
+</div>
+<nav class="sidebar">
+    <div class="logo-container">
+        <img src="images/talland-college-logo.png" alt="Logo" class="logo">
+    </div>
+    <ul>
+        <li><a href="/"><i class="fa fa-fw fa-home"></i> Overzicht</a></li>
+        <li><a href="#home"><i class="fa-solid fa-circle-info"></i> Informatie</a></li>
+        <li><a href="#home"><i class="fa-solid fa-right-from-bracket"></i> Inloggen</a></li>
+    </ul>
+</nav>
