21701b56eb9cedc6d299678a57d071c5c70634e9
Chris <166389@student.horizoncollege.nl>
2024-06-17 11:45:11 +0200

    Profiel pagina sidebar
    - De sidebar was niet zichtbaar op de profiel pagina, dit is nu opgelost

--- registratie-applicatie/resources/views/auth/login.blade.php
+++ registratie-applicatie/resources/views/auth/login.blade.php
@@ -28,7 +28,7 @@
                         <input id="remember_me" type="checkbox"
                             class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
                             name="remember">
-                        <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                        <span class="ms-2 text-sm text-gray-600">{{ __('Gegevens onthouden') }}</span>
                     </label>
                 </div>
                 <div class="btm-row">
